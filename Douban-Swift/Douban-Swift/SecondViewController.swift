5f0da48b065089dc5988268f96463e2589481e6b
D <hermit.dove@gmail.com>
2015-12-21 16:26:53 +0800

    start :) the init

---
+++ Douban-Swift/Douban-Swift/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Douban-Swift
+//
+//  Created by Doye on 15/12/21.
+//  Copyright © 2015年 d0ye. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
